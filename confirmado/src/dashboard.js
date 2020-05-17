da09572168f47c23e08cc4181a97f3594e6afd20
Emiliano Elói Silva Barbosa <emiliano.barbosa@g.globo>
2020-05-16 23:41:51 -0300

    wip: atualizando o gráfico

--- confirmado/src/dashboard.js
+++ confirmado/src/dashboard.js
@@ -1,9 +1,44 @@
 /* globals Chart:false, feather:false */
-
 (function () {
   'use strict'
 
   feather.replace()
+
+  var dataset = function(label, data, color) {
+    var defaultLabel = label  
+    var defaultData = data
+    var defaultDataset = {
+      data: defaultData,
+      lineTension: 0,
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff'
+    }
+    return {
+      label: defaultLabel,
+      data: defaultData,
+      dataset: defaultDataset
+    }
+  }
+
+  var dataUSA = dataset(
+    ['2020-01-21', '2020-01-22', '2020-01-23'] ,
+    [1, 1, 1],
+    '#c1c1c1'
+  )
+
+  var dataBRA = dataset(
+    ['2020-01-21', '2020-01-22', '2020-01-23'] ,
+    [0, 0, 0],
+    '#c1c1c1'
+  )
+
+  var dataCHI = dataset(
+    ['2020-01-21', '2020-01-22', '2020-01-23'] ,
+    [0, 558, 643],
+    '#c1c1c1'
+  )
 
   // Graphs
   var ctx = document.getElementById('myChart')
@@ -11,31 +46,12 @@
   var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
+      labels:dataUSA.label,
+      datasets: [
+        dataUSA.dataset,
+        dataBRA.dataset,
+        dataCHI.dataset
+      ]
     },
     options: {
       scales: {
