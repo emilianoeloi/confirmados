0b253971ea6c5b7ac7abe6ff3a29cef8987680f0
Emiliano Elói Silva Barbosa <emiliano.barbosa@g.globo>
2020-05-27 22:57:58 -0300

    wip: consturindo o script de integração

--- integration/index.js
+++ integration/index.js
@@ -5,6 +5,39 @@ const path = require('path');
 var cases2020_01_21 = path.join(__dirname, 'csse_covid_19_daily_reports/01-22-2020.csv')
 var casesAll = path.join(__dirname,'casesAll.js');
 
+function csseCovid19DailyReports(csv) {
+   var countries = {}
+   var lines=csv.split("\n");
+   var result = [];
+   var headers=lines[0].split(",");
+   for(var i=1;i<lines.length;i++){
+      var obj = {};
+      var objCountry = {};
+      var currentline=lines[i].split(",");
+      var countriesResult = []
+      //Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered
+      for(var j=0;j<headers.length;j++){
+         //2, 3
+         if (j == 2 || j == 3) { 
+            if (isNaN(currentline[j])) {
+               obj[headers[j]] = currentline[j];
+            } else {
+               obj[headers[j]] = parseInt(currentline[j]);
+            }
+         }
+      }
+      if (countries[currentline[1]]) {
+         countries[currentline[1]].push(obj)
+      } else {
+         countries[currentline[1]] = new Array()
+         countries[currentline[1]].push(obj)
+      }
+      result.push(obj);
+   }
+  console.info(countries)
+  return JSON.stringify(result);
+}
+
 function csvJSON(csv){
 
     var lines=csv.split("\n");
@@ -31,7 +64,7 @@ fs.readFile(cases2020_01_21, function(err, data) {
    if (err) {
       return console.error(err);
    }
-   var csvToJSON = csvJSON(data.toString()) 
+   var csvToJSON = csseCovid19DailyReports(data.toString()) 
    fs.writeFile(casesAll, csvToJSON, function(err) {
       if (err) {
          return console.error(err);
