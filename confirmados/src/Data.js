617d9b62d92e9a1eff5c4a89e36f2218feb6d6f3
Emiliano Elói Silva Barbosa <emiliano.barbosa@g.globo>
2020-05-28 22:32:28 -0300

    wip: atualizando o script cara criar um arquivos para todoso os paises do mundo

--- confirmados/src/Data.js
+++ confirmados/src/Data.js
@@ -1,12 +1,14 @@
 import { Loader } from './Loader.js'
-import brazil from './casesBrazil.js'
-import china from './casesChina.js'
-import usa from './casesUSA.js'
+import brazil from './casesCountries/casesBrazil.js'
+import china from './casesCountries/casesChina.js'
+import usa from './casesCountries/casesUSA.js'
+import mainlandChina from './casesCountries/casesMainland_China.js'
 
 let loader = new Loader()
-loader.setCaseCountry(usa.country, usa.cases)
-//loader.setCaseCountry(china.country, china.cases)
-//loader.setCaseCountry(brazil.country, brazil.cases)
+
+//loader.setCaseCountry(usa.country, usa.cases)
+loader.setCaseCountry(mainlandChina.country, mainlandChina.cases)
+
 
 console.info("MAX: " +loader.max)
 
---
+++ confirmados/src/casesCountries/casesMainland_China.js
@@ -0,0 +1,19 @@
+let country = {
+    name: 'Mainland China',
+    color: '#000000'
+}
+
+let cases = [
+    { date: '2020-01-21', cases: 547, "deaths": 0},
+    { date: '2020-01-22', cases: 639, "deaths": 0},
+    { date: '2020-01-23', cases: 916, "deaths": 0},
+    { date: '2020-01-24', cases: 1399, "deaths": 0},
+    { date: '2020-01-25', cases: 2062, "deaths": 0},
+    { date: '2020-01-26', cases: 2863, "deaths": 0},
+    { date: '2020-01-27', cases: 5494, "deaths": 0}
+]
+
+export default {
+    country,
+    cases
+}
