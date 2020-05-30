f56d22ed855770823c9091168e40704432df1b50
Emiliano Elói Silva Barbosa <emiliano.barbosa@g.globo>
2020-05-29 23:10:22 -0300

    wip: criando arquivos para países

--- integrate/index.js
+++ integrate/index.js
@@ -7,8 +7,8 @@ var casesAll = path.join(__dirname,'casesAll.js');
 
 // from: 21/01/2020 to: Now
 function dateIntervalCOVID19() {
-   var iniDate = new Date("2020-01-21T00:00:00")
-   var endDate = new Date("2020-01-26T00:00")// Date.now()
+   var iniDate = new Date("2020-01-22T00:00:00")
+   var endDate = new Date("2020-01-22T00:00")// Date.now()
 
    var loop = new Date(iniDate);
    while(loop <= endDate){
@@ -66,29 +66,7 @@ function csseCovid19DailyReports(csv) {
       result.push(obj);
    }
   console.info(countries)
-  return JSON.stringify(result);
-}
-
-function csvJSON(csv){
-
-    var lines=csv.split("\n");
-    var result = [];
-    var headers=lines[0].split(",");
-
-    for(var i=1;i<lines.length;i++){
-        var obj = {};
-        var currentline=lines[i].split(",");
-        for(var j=0;j<headers.length;j++){
-           if (isNaN(currentline[j])) {
-            obj[headers[j]] = currentline[j];
-           } else {
-            obj[headers[j]] = parseInt(currentline[j]);
-           }
-        }
-        result.push(obj);
-    }
-
-    return JSON.stringify(result);
+  return countries;
 }
 
 function whiteFile(csvFile) {
@@ -97,10 +75,17 @@ function whiteFile(csvFile) {
          return console.error(err);
       }
       var csvToJSON = csseCovid19DailyReports(data.toString()) 
-      fs.writeFile(casesAll, csvToJSON, function(err) {
+      saveCountriesData(csvToJSON)
+   });
+}
+
+function saveCountriesData(data) {
+   Object.values(data).forEach(function(a, b) {
+      var countryFile = `countries/cases${Object.keys(data)[b]}.js`
+      fs.writeFile(countryFile, JSON.stringify(a), function(err) {
          if (err) {
             return console.error(err);
          }
       });
-   });
+   })
 }
