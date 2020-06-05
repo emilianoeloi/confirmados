b8c788871d5ab56363141f5dda183921d8f173de
Emiliano Elói Silva Barbosa <emiliano.barbosa@g.globo>
2020-06-04 22:56:57 -0300

    wip: refatorando o saveCountriesData

--- integrate/saveCountriesData.js
+++ integrate/saveCountriesData.js
@@ -1,25 +1,37 @@
 // saveCountriesData.js
 
-function saveCountriesData(data, writeFileCB) {
-   var globalCovid19 = JSON.parse(process.env.GLOBAL_COVID_29)
-   Object.values(data).forEach(function(a, b) {
-      var countryFile = Object.keys(data)[b];
-      Object.values(data).forEach(function(c, d) {
-         var globalCountryFile = Object.keys(globalCovid19)[d]
-         if (globalCountryFile == countryFile) {
-            if (globalCovid19[countryFile][0].date != a[0].date) {
-               globalCovid19[countryFile].push(a[0])
-            }  
-         } else {
-            globalCovid19[countryFile] = [a[0]];
-         }
-      })
-   })
+const dbCovid19 = function(countryFile, cases) {
+   let globalCovid19 = JSON.parse(process.env.GLOBAL_COVID_29)
+   let countriesDB = Object.values(globalCovid19);
 
-   console.info(globalCovid19)
+   let flag = true
+   countriesDB.forEach(function(value, key) {
+      var globalCountryFile = Object.keys(globalCovid19)[key]
+      if (globalCountryFile == countryFile) {
+         globalCovid19[countryFile].push(value[0])
+         flag = false  
+      }
+   })
+   if (flag) {
+      globalCovid19[countryFile] = [cases[0]]
+   }
 
    process.env.GLOBAL_COVID_29 = JSON.stringify(globalCovid19)
-   writeFileCB()
- }
+}
 
- module.exports = saveCountriesData
+function saveCountriesData(countriesData, writeFileCB) {
+
+   const constries = Object.values(countriesData);
+
+   constries.forEach(function(value, key) {
+      var countryFile = Object.keys(countriesData)[key];
+      // console.info('countryFile', countryFile)
+      // console.info('value', value)
+      // console.info('key', key)
+      dbCovid19(countryFile, value)
+   })
+
+   writeFileCB()
+}
+
+module.exports = saveCountriesData
