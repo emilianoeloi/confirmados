3e336fe89d111b9fdd2186a60e780fc780903921
Emiliano Elói Silva Barbosa <emiliano.barbosa@g.globo>
2020-09-13 18:14:20 -0300

    bump: atualizando uma versão nova. 1.0.0-alpha.1 até 12/09/2020

--- integrate/index.js
+++ integrate/index.js
@@ -13,8 +13,8 @@ var csseCovid19DailyReport = path.join(__dirname, 'csse_covid_19_daily_reports')
 getCSVRequestFiles(
    csseCovid19DailyReport,
    new Date("2020-01-22T00:00:00.000"),
-   new Date("2020-06-12T23:59:59.999"),
-   ["Argentina", "Brazil", "Paraguay", "Uruguay", "Venezuela"],
+   new Date("2020-09-12T23:59:59.999"),
+   ["India"],
    readFile.read,
    writeFile.writeFile
 );
