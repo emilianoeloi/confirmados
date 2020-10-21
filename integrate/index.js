8c25a6a49ae1ffc7812f9f89fcf8fdd506f929a1
Emiliano Barbosa <emilianoeloi@gmail.com>
2020-10-20 21:10:51 -0300

    wip: Atualizando Confirmados: 19/10/2020

--- integrate/index.js
+++ integrate/index.js
@@ -13,14 +13,18 @@ const csseCovid19DailyReport = path.join(__dirname, 'csse_covid_19_daily_reports
 
 const argentina = new Country("argentina", "Argentina", "#75AADB")
 const brazil = new Country("brazil", "Brazil", "#009c3b")
-const contriesToRequest = [brazil, argentina]
+
+const spain = new Country("spain", "Spain", "#AA151B")
+const italy = new Country("italy", "Italy", "#008C45")
+
+const contriesToRequest = [spain, italy]
 
 writeFile.writeIntegrateFile(contriesToRequest)
 
 getCSVRequestFiles(
    csseCovid19DailyReport,
    new Date("2020-01-22T00:00:00.000"),
-   new Date("2020-10-18T23:59:59.999"),
+   new Date("2020-10-19T23:59:59.999"),
    contriesToRequest,
    readFile.read,
    writeFile.writeFile
