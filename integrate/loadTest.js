e06fe83a8ec4eeb42052425bcb8d1334f07e7c06
Emiliano Barbosa <emiliano.barbosa@g.globo>
2020-12-19 21:24:47 -0300

    wip: brasil

--- integrate/loadTest.js
+++ integrate/loadTest.js
@@ -2,8 +2,6 @@ const State = require('./State.js')
 const Load = require('./Load.js')
 const integrateStates = require('./Paises/Brazil/integrate.json')
 
-const turkey = new Country("turkey", "Turkey", "#E30A17")
-
 const init = new Date("2020-06-01T00:00:00.000")
 const end = new Date("2020-12-18T23:59:59.999")
 const norte = new Load(
