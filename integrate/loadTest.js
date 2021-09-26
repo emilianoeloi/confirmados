e9e7334a23039e4b103b51f108a52a99b4c1fd3d
Emiliano Elói Silva Barbosa <emilianoeloi@gmail.com>
2021-09-26 16:08:29 -0300

    wip: Atualizando para 25/09/2021

--- integrate/loadTest.js
+++ integrate/loadTest.js
@@ -3,8 +3,8 @@
 const Load = require('./Load.js')
 const integrateStates = require('./Paises/Brazil/integrate.json')
 
-const init = new Date("2020-09-06T00:00:00.000")
-const end = new Date("2021-09-06T23:59:59.999")
+const init = new Date("2020-09-25T00:00:00.000")
+const end = new Date("2021-09-25T23:59:59.999")
 const norte = new Load(
     "0",
     integrateStates,
