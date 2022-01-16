ee45d2457d118efcc4fbd8581cfedd130ee12c20
Emiliano Elói Silva Barbosa <emilianoeloi@gmail.com>
2022-01-16 10:15:20 -0300

    Domingo, 16 de janeiro de 2022

--- integrate/loadTest.js
+++ integrate/loadTest.js
@@ -9,18 +9,16 @@ class Load {
     }
 }
 
-// Domingo, 28 de novembro de 2012
-//
 const us = new Country("us", "US", "#3C3B6E")
-const germany = new Country("germany", "Germany", "#000000")
-const uk = new Country("united_kingdom", "United Kingdom", "#00247D")
-const turkey = new Country("turkey", "Turkey", "#E30A17")
 const france = new Country("france", "France", "#1C4992")
-const spain = new Country("spain", "Spain", "#AA151B")
+const uk = new Country("united_kingdom", "United Kingdom", "#00247D")
 const italy = new Country("italy", "Italy", "#008C45")
+const spain = new Country("spain", "Spain", "#AA151B")
+const india = new Country("india", "India", "#FF9933")
+const argentina = new Country("argentina", "Argentina", "#75AADB")
 
-const init = new Date("2021-01-08T00:00:00.000")
-const end = new Date("2022-01-08T23:59:59.999")
+const init = new Date("2020-06-01T00:00:00.000")
+const end = new Date("2022-01-15T23:59:59.999")
 
 const top7 = new Load(
     "Top 7",
@@ -30,8 +28,8 @@ const top7 = new Load(
         uk,
         italy,
         spain,
-        germany,
-        turkey
+        india,
+        argentina
     ],
     init,
     end
