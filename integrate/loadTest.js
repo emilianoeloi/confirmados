025986d9fb3ca13da6375cb21c90fdf06dedcd5b
Emiliano Elói Silva Barbosa <emilianoeloi@gmail.com>
2022-02-09 16:17:06 -0300

    update: 08/02/2022

--- integrate/loadTest.js
+++ integrate/loadTest.js
@@ -12,24 +12,24 @@ class Load {
 const us = new Country("us", "US", "#3C3B6E")
 const france = new Country("france", "France", "#1C4992")
 const india = new Country("india", "India", "#FF9933")
+const brazil = new Country("brazil", "Brazil", "#00ff00")
 const italy = new Country("italy", "Italy", "#008C45")
+const germany = new Country("germany", "Germany", "#000000")
 const uk = new Country("united_kingdom", "United Kingdom", "#00247D")
-const spain = new Country("spain", "Spain", "#AA151B")
-const argentina = new Country("argentina", "Argentina", "#75AADB")
 
-const init = new Date("2021-01-22T00:00:00.000")
-const end = new Date("2022-01-22T23:59:59.999")
+const init = new Date("2021-02-08T00:00:00.000")
+const end = new Date("2022-02-08T23:59:59.999")
 
 const top7 = new Load(
     "Top 7",
     [
         us,
         france,
-        uk,
-        italy,
-        spain,
         india,
-        argentina
+        brazil,
+        italy,
+        germany,
+        uk
     ],
     init,
     end
